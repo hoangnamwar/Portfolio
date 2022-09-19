2c3ca928b033117245772ccf3238848e2d8b4e41
hoang nam <davidvunam@gmail.com>
2022-09-20 00:37:16 +0700

    update: 19/9/22

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,62 +1,72 @@
-import React from 'react'
-import Container from '@mui/material/Container'
-import { Box, Typography, useTheme } from '@mui/material'
-import Link from '@mui/material/Link';
+import React from "react";
+import Container from "@mui/material/Container";
+import { Box, Typography, useTheme } from "@mui/material";
+import Link from "@mui/material/Link";
 
 const Footer = () => {
+  const theme = useTheme();
 
-    const theme = useTheme();
+  return (
+    <Box
+      sx={{
+        bgcolor: `${theme.palette.secondary.main}`,
+        position: "fixed",
+        bottom: 0,
+        width: "100%",
+      }}
+    >
+      <Container maxWidth="lg">
+        <Typography
+          variant="body1"
+          className="footer-text"
+          sx={{ color: `${theme.status.txt}` }}
+        >
+          @Idea from
+          <Link
+            sx={{
+              color: `${theme.status.txt}`,
+              fontStyle: "oblique",
+              fontWeight: 200,
+              padding: "5px",
+            }}
+            href="https://soumyajit.tech/"
+            target="_blank"
+          >
+            Soumyajit Behera{" "}
+          </Link>
+          and
+          <Link
+            sx={{
+              color: `${theme.status.txt}`,
+              fontStyle: "oblique",
+              fontWeight: 200,
+              padding: "5px",
+            }}
+            href="https://developer-portfolio-1hanzla100.vercel.app/"
+            target="_blank"
+          >
+            Hanzla
+          </Link>
+          <span style={{ padding: "50px" }}></span>
+          @Design and develop from Hoang Nam
+          <span style={{ padding: "50px" }}></span>
+          Sourcode on
+          <Link
+            sx={{
+              color: `${theme.status.txt}`,
+              fontStyle: "oblique",
+              fontWeight: 200,
+              padding: "5px",
+            }}
+            href="https://github.com/hoangnamwar/Portfolio/tree/main"
+            target="_blank"
+          >
+            Github
+          </Link>
+        </Typography>
+      </Container>
+    </Box>
+  );
+};
 
-    return (
-        // <Box sx={{ bgcolor: ((theme.palette.mode === 'dark') ? `${theme.palette.secondary.dark}` : `${theme.palette.secondary.light}`), 
-        //             marginTop: '100px' }}> 
-
-        <Box sx={{ bgcolor: `${theme.palette.secondary.main}`, marginTop: '100px' }}>
-            <Container maxWidth="lg">
-                <Typography
-                    variant="body1"
-                    className="footer-text"
-                    sx={{ color: `${theme.status.txt}` }}
-                >
-                    @Idea from 
-                    <Link 
-                        sx={{ 
-                            color: `${theme.status.txt}`,
-                            fontStyle: 'oblique',
-                            fontWeight: 200,
-                            padding: '5px'
-                        }}
-                        href="https://soumyajit.tech/"
-                    >Soumyajit Behera </Link> 
-                    and 
-                        <Link 
-                        sx={{ 
-                            color: `${theme.status.txt}`, 
-                            fontStyle: 'oblique',
-                            fontWeight: 200,
-                            padding: '5px'
-
-                        }}
-                        href="https://developer-portfolio-1hanzla100.vercel.app/"
-                    >Hanzla</Link> 
-                    <span style={{padding: '50px'}}></span>
-                    @Design and develop from Hoang Nam
-                    <span style={{padding: '50px'}}></span>
-                    Sourcode on
-                    <Link 
-                        sx={{ 
-                            color: `${theme.status.txt}`, 
-                            fontStyle: 'oblique',
-                            fontWeight: 200,
-                            padding: '5px'
-
-                        }}
-                        href="https://developer-portfolio-1hanzla100.vercel.app/"
-                    >Github</Link> 
-                </Typography>
-            </Container>
-        </Box>
-    )
-}
-
-export default Footer
+export default Footer;
--- src/components/Preloader.tsx
+++ src/components/Preloader.tsx
@@ -1,8 +1,18 @@
+import { useTheme } from "@mui/material";
 import React from "react";
 
-const Preloader = () => 
-{
-  return <div id="preloader"></div>;
-}
+const Preloader = () => {
+  const theme = useTheme();
+  return (
+    <div
+      id="preloader"
+      style={{
+        backgroundColor: `${
+          theme.palette.mode === "dark" ? "#0c0513" : "#fff"
+        }`,
+      }}
+    ></div>
+  );
+};
 
 export default Preloader;
